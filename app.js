48791eb7e85bec7d5ce545832b6eac605011db6b
Sloan <sloanireland1@gmail.com>
2023-09-27 22:13:39 -0700

    added map API from open street

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 import "style.css";
 
+var map = L.map('map').setView([34.052235, -118.243683], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

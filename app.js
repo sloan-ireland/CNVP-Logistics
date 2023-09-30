da7f0585b3706e1f6d5c76dd0795f3a8925c1009
Sloan <sloanireland1@gmail.com>
2023-09-30 05:58:14 -0700

    one file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import "./style.css";
+/*import "./style.css";
 
 var mymap = L.map('mapid').setView([34.052235, -118.243683], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map);*/

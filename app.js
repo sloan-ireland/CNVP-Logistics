d1db4c2f0563ff97b31e4deefcdbfbb4816fee36
Sloan <sloanireland1@gmail.com>
2023-09-30 05:54:09 -0700

    Revert "consolidation to one file"
    This reverts commit ca9dddeec5091c1ac27409bfe2fea29b15e372d8.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var mymap = L.map('mapid').setView([34.052235, -118.243683], 13);
+import "./style.css";
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+var mymap = L.map('mapid').setView([34.052235, -118.243683], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

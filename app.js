2d81d8b5b3e0e47282b1133e2d49cc0b364919be
Sloan <sloanireland1@gmail.com>
2023-10-03 11:03:52 -0700

    testing pop up markers

--- app.js
+++ app.js
@@ -19,3 +19,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var maxZoomToSeeBorders = mymap.getBoundsZoom(californiaBounds);
 mymap.setMinZoom(maxZoomToSeeBorders); // Prevents the user from zooming farther out than the bounds
+
+
+var tribalLocations = [
+    { name: "Agua Caliente Band of Cahuilla Indians", lat: 33.809212, lon: -116.525694 },
+    { name: "Augustine Band of Cahuilla Indians", lat: 33.685378, lon: -116.142309 },
+    { name: "Barona Band of Mission Indians", lat: 32.976976, lon: -116.853185 },
+    { name: "Bear River Band of Rohnerville Rancheria", lat: 40.699806, lon: -124.234920 },
+    { name: "Berry Creek Rancheria", lat: 39.492117, lon: -121.536500 },
+    { name: "Big Lagoon Rancheria", lat: 41.313362, lon: -124.006708 },
+    { name: "Big Pine Paiute Tribe of the Owens Valley", lat: 37.167570, lon: -118.392080 },
+    { name: "Big Sandy Rancheria", lat: 36.938469, lon: -119.503935 },
+    { name: "Big Valley Band of Pomo Indians", lat: 39.091760, lon: -122.959297 },
+    { name: "Bishop Paiute Tribe", lat: 37.363872, lon: -118.395134 }
+    // Add more locations here
+];
+
+//add markers with popups
+for (var i = 0; i < tribalLocations.length; i++) {
+    var location = tribalLocations[i];
+    L.marker([location.lat, location.lon], { icon: blueIcon })
+        .bindPopup(location.name)
+        .addTo(map);
+}
+
+//custom blue icon
+var blueIcon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x-blue.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});

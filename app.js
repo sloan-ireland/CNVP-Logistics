5fc9af851643e166b31c588625690bf0b5386090
natalia1615 <nhernandez2@oxy.edu>
2023-10-19 15:38:28 -0700

    adding info to markers

--- app.js
+++ app.js
@@ -40,11 +40,24 @@ var markers = [
     },
     {
         coords: [38.590576, -121.489906],
-        popupText: ['Buena Vista Rancheria of Me-Wuk Indians']
+        popupText: ['Buena Vista Rancheria of Me-Wuk Indians',
+                    'Rhonda Morningstar Pope (Chairperson)',
+                    'Tel: (916) 491-0011',
+                    'Fax: (916) 491-0012',
+                    'Recognition Status: Federal',
+                    '1418 20th Street, Suite 200 Sacramento, CA95811-5206'
+    ]
     },
     {
         coords: [33.680302, -116.173897],
-        popupText: ['Augustine Band of Cahuilla Indians']
+        popupText: ['Augustine Band of Cahuilla Indians',
+                    'Amanda Vance (Chairperson)',
+                    'Tel: (760) 398-4722',
+                    'Fax: (760) 369-7161',
+                    'Recognition Status: Federal',
+                    'PO Box 846',
+                    'Coachella, CA92236-0846'
+                ]
     },
     {
         coords: [41.4871, -120.5425],
@@ -59,67 +72,179 @@ var markers = [
     },
     {
         coords: [32.8573, -116.9222],
-        popupText: ['Barona Band of Mission Indians']
+        popupText: ['Barona Band of Mission Indians',
+                    'Edwin "Thorpe" Romero (Chairman)',
+                    'Tel: (619) 443-6612',
+                    'Fax: (619) 443-0681',
+                    'Recognition Status: Federal',
+                    '1095 Barona Rd Lakeside, CA92040-1541'
+    ]
     },
     {
         coords: [40.6411, -124.2263],
-        popupText: ['Bear River Band of Rohnerville Rancheria']
+        popupText: ['Bear River Band of Rohnerville Rancheria',
+                    'Josafina Cortez (Chairwoman)',
+                    'Tel: (707) 733-1900',
+                    'Fax: (707) 733-1723',
+                    'Recognition Status: Federal',
+                    '266 Keisner Rd Loleta, CA95551-9646'
+    ]
     },
     {
         coords: [37.8183, -118.4770],
-        popupText: ['Benton Paiute - U-tu Utu Gwaitu Paiute Tribe']
+        popupText: ['Benton Paiute - U-tu Utu Gwaitu Paiute Tribe',
+                    'Shane Saulque (Chairman)',
+                    'Tel: (760) 933-2321',
+                    'Fax: (760) 933-2412',
+                    'Recognition Status: Federal',
+                    '25669 Highway 6, PMB 1 Benton, CA93512-9703'
+
+                ]
     },
     {
         coords: [39.511608, -121.556725],
-        popupText: ['Berry Creek Rancheria of Maidu Indians of California']
+        popupText: ['Berry Creek Rancheria of Maidu Indians of California',
+                    'Francis Steele, Jr. (Chairman)',
+                    'Tel: (530) 534-3859',
+                    'Fax: (530) 534-1151',
+                    'Recognition Status: Federal',
+                    '5 Tyme Way',
+                    'Oroville, CA95966-9115'
+
+    ]
     },
     {
         coords: [41.0593, -124.1431],
-        popupText: ['Big Lagoon Rancheria']
+        popupText: ['Big Lagoon Rancheria',
+                    'Virgil Moorehead (Chairman)',
+                    'Tel: (707) 826-2079',
+                    'Fax: (707) 826-0495',
+                    'Recognition Status: Federal',
+                    'PO Box 3060',
+                    'Trinidad, CA95570-3060'
+    ]
     },
     {
         coords: [37.1649, -118.2895],
-        popupText: ['Big Pine Paiute Tribe of the Owens Valley']
+        popupText: ['Big Pine Paiute Tribe of the Owens Valley',
+                    'Shannon Romero (Tribal Chairperson)',
+                    'Tel: (760) 938-2003',
+                    'Fax: (760) 938-2942',
+                    'Recognition Status: Federal',
+                    'PO Box 700',
+                    'Big Pine, CA93513-0700'
+    ]
     },
     {
         coords: [37.0808, -119.4854],
-        popupText: ['Big Sandy Rancheria']
+        popupText: ['Big Sandy Rancheria',
+                    'Elizabeth Kipp (Chairperson)',
+                    'Tel: (559) 855-4003',
+                    'Fax: (559) 855-4129',
+                    'Recognition Status: Federal',
+                    'PO Box 337',
+                    'Auberry, CA93602-0337'
+    ]
     },
     {
         coords: [39.0430, -122.9158],
-        popupText: ['Big Valley Rancheria']
+        popupText: ['Big Valley Rancheria',
+                    'Philip Gomez (Chairman)',
+                    'Tel: (707) 263-3924',
+                    'Fax: (707) 533-2941',
+                    'Recognition Status: Federal',
+                    '2726 Mission Rancheria Rd',
+                    'Lakeport, CA95453-9612'
+    ]
     },
     {
         coords: [37.3614, -1118.3952],
-        popupText: ['Bishop Paiute Trib']
+        popupText: ['Bishop Paiute Tribe',
+                     'Brian Poncho (Interim Chairperson)',
+                     'Tel: (760) 873-3584',
+                     'Fax: (760) 873-4143',
+                     'Recognition Status: Federal',
+                     '50 N Tu Su Ln',
+                     'Bishop, CA93514-8058'
+    ]
     },
     {
         coords: [40.8829, -123.9839],
-        popupText: ['Blue Lake Rancheria']
+        popupText: ['Blue Lake Rancheria',
+                    'Claudia Brundin (Chairperson)',
+                    'Tel: (707) 668-5101',
+                    'Fax: (707) 668-4272',
+                    'Recognition Status: Federal',
+                    'PO Box 428',
+                    'Blue Lake, CA95525-0314'
+    ]
     },
     {
         coords: [38.258308, -119.2314],
-        popupText: ['Bridgeport Paiute Indian Colony']
+        popupText: ['Bridgeport Paiute Indian Colony',
+                    'John Glazier (Chairman)',
+                    'Tel: (760) 932-7083',
+                    'Fax: (760) 932-7846',
+                    'Recognition Status: Federal',
+                    'PO Box 37',
+                    'Bridgeport, CA93517-0037'
+    ]
     },
     {
         coords: [38.5816, -121.4944],
-        popupText: ['Buena Vista Rancheria of Me-Wuk Indians']
+        popupText: ['Buena Vista Rancheria of Me-Wuk Indians',
+                    'Rhonda Morningstar Pope (Chairperson)',
+                    'Tel: (916) 491-0011',
+                    'Fax: (916) 491-0012',
+                    'Recognition Status: Federal',
+                    '1418 20th Street, Suite 200',
+                    'Sacramento, CA95811-5206',
+    ]
     },
     {
         coords: [33.7206, -116.2156],
-        popupText: ['Cabazon Band of Mission Indians']
+        popupText: ['Cabazon Band of Mission Indians',
+                    'Doug Welmas (Chairman)',
+                    'Tel: (760) 342-2593',
+                    'Fax: (760) 347-7880',
+                    'Recognition Status: Federal',
+                    '84-245 Indio Springs Parkway',
+                    'Indio, CA92203-'
+    ]
     },
     {
         coords: [39.2143, -122.0094],
-        popupText: ['Cachil Dehe Band of Wintun Indians of the Colusa Indian Rancheria']
+        popupText: ['Cachil Dehe Band of Wintun Indians of the Colusa Indian Rancheria',
+                    'Daniel Gomez(Chairman)',
+                    'Tel: (530) 458-8231',
+                    'Fax: (530) 458-3866',
+                    'Recognition Status: Federal',
+                    '3730 State Highway 45',
+                    'Colusa, CA95932-4022'
+        ]
+
     },
     {
         coords: [39.6882, -123.4828],
-        popupText: ['Cahto Tribe of the Laytonville Rancheria']
+        popupText: ['Cahto Tribe of the Laytonville Rancheria',
+                    'Richard J. Smith (Chairperson)',
+                    'Tel: (707) 984-6197',
+                    'Fax: (707) 984-6201',
+                    'Recognition Status: Federal',
+                    'PO Box 1239',
+                    'Laytonville, CA95454-1239'
+    ]
     },
     {
         coords: [33.5553, -116.6742],
-        popupText: ['Cahuilla Band of Mission Indians of the Cahuilla Reservation']
+        popupText: ['Cahuilla Band of Mission Indians of the Cahuilla Reservation',
+                    'Daniel Salgado (Chairman)',
+                    'Tel: (951) 763-5549',
+                    'Fax: (951) 763-2808',
+                    'Recognition Status: Federal',
+                    'PO Box 391760',
+                    'Anza, CA92539-1760'
+    ]
     }
     // Add more markers here
 ];

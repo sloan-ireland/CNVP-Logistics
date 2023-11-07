f963258628d1a95509cb51e5fbaa1fd74888ec45
Sloan Ireland <sloanireland1@gmail.com>
2023-11-07 14:47:26 -0800

    trying to fix map display problem

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 var californiaBounds = [
     [32.5, -124.5], // Southwest corner of California
-    [42, -114]     // Northeast corner of California
-];
-
-
-// Initialize the map with maxBounds
-var mymap = L.map('mapid', {
+    [42, -114], // Northeast corner of California
+  ];
+  
+  var mymap = L.map("mapid", {
     maxBounds: californiaBounds,
-    maxBoundsViscosity: 1.0, // Keeps the user within the bounds
-    maxZoom: 10, // Set the maximum zoom level
-    minZoom: 6   // Set the minimum zoom level
-}).setView([36.7783, -119.4179], 6); // Centered on California with an appropriate zoom level
-
-// Add a tile layer 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
-
-
+    maxBoundsViscosity: 1.0,
+    maxZoom: 10,
+    minZoom: 6,
+  }).setView([36.7783, -119.4179], 6);
+  
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(mymap);
 
 //add the tribal borders to the map
 fetch('Federally_Recognized_Tribal_Lands.geojson')

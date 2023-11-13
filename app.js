d02e5d8f34db42257f6bdbdbfb20f12cfb542159
natalia1615 <nhernandez2@oxy.edu>
2023-11-13 13:24:07 -0800

    fix error test 3

--- app.js
+++ app.js
@@ -953,7 +953,7 @@ var markers = [
     ]
     },
     {
-        coords: [33.2183701, --117.0341967],
+        coords: [33.2183701, -117.0341967],
         popupText: ['San Pasqual Band of Diegueno Indians',
                     'Allen Lawson (Tribal Chairman)',
                     'Allen Lawson (Tribal Chairman)',
@@ -1243,7 +1243,7 @@ var markers = [
                     'Klamath, CA95548-1027'
 
     ]
-    },
+    }
     
 
     // Add more markers here
